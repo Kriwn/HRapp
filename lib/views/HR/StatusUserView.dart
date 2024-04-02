d082321144e6db2db8562d4a47edf18f0091d8a0
Kriwn <nonisgod@gmail.com>
2024-04-03 06:44:29 +0700

    done

--- lib/views/HR/StatusUserView.dart
+++ lib/views/HR/StatusUserView.dart
@@ -1,5 +1,9 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:hr/models/history.dart';
 import 'package:hr/models/user.dart';
+import 'package:hr/services/Auth.dart';
 import 'package:hr/services/UserDB.dart';
 
 class StatusUser extends StatefulWidget {
@@ -9,24 +13,22 @@ class StatusUser extends StatefulWidget {
 
   @override
   _StatusUser createState() => _StatusUser(data: data);
-  
 }
 
 class _StatusUser extends State<StatusUser> {
   // Future<List<String>>? _UsernameList;
-  
+
   final dynamic data;
-  
+
   _StatusUser({required this.data});
 
   @override
-  void initState(){
+  void initState() {
     super.initState();
     // _UsernameList = DepartmentController().getNameByDepartment(data);
   }
-  
 
-@override
+  @override
   Widget build(BuildContext context) {
     double screenHeight = MediaQuery.of(context).size.height;
     return Scaffold(
@@ -76,12 +78,12 @@ class _StatusUser extends State<StatusUser> {
                   width: MediaQuery.of(context).size.width * 0.3,
                   height: MediaQuery.of(context).size.width * 0.3,
                   child: ClipRRect(
-                    child :Image.network(url,
-                    fit: BoxFit.cover,
+                    child: Image.network(
+                      url,
+                      fit: BoxFit.cover,
                     ),
                     borderRadius: BorderRadius.circular(20),
-                    
-                    ),
+                  ),
                 ),
                 const SizedBox(
                   height: 20,
@@ -172,7 +174,76 @@ class _StatusUser extends State<StatusUser> {
                         ],
                       ),
                       const SizedBox(height: 20),
-                      
+                      SingleChildScrollView(
+                        child: Padding(
+                          padding: const EdgeInsets.fromLTRB(20, 5, 20, 5),
+                          child: Container(
+                            clipBehavior: Clip.antiAlias,
+                            decoration: BoxDecoration(
+                                border: Border.all(color: Colors.grey),
+                                borderRadius: BorderRadius.circular(20),
+                                color: const Color.fromARGB(255, 240, 240, 240),
+                                boxShadow: [
+                                  BoxShadow(
+                                      color: Colors.grey.withOpacity(0.7),
+                                      spreadRadius: 4,
+                                      blurRadius: 5,
+                                      offset: const Offset(0, 1.75))
+                                ]),
+                            height: 0.5 * screenHeight,
+                            child: StreamBuilder(
+                                stream: FirebaseFirestore.instance
+                                    .collection("User")
+                                    .doc(userId)
+                                    .collection("History")
+                                    .snapshots(),
+                                builder: (context, snapshot) {
+                                  List historyList = snapshot.data?.docs ?? [];
+                                  return ListView.builder(
+                                      itemCount: historyList.length,
+                                      itemBuilder: (context, index) {
+                                        // print("asdasdadasdasdasas${historyList[index].data()}");
+                                        History history = History.fromJson(
+                                            historyList[index].data());
+                                        return Padding(
+                                            padding: const EdgeInsets.symmetric(
+                                              vertical: 5,
+                                              horizontal: 10,
+                                            ),
+                                            child: Container(
+                                              decoration: BoxDecoration(
+                                                  color: Colors.amber,
+                                                  borderRadius:
+                                                      BorderRadius.circular(15),
+                                                  border: Border.all(
+                                                      color: Colors.yellow,
+                                                      width: 2.0)),
+                                              child: ListTile(
+                                                shape: RoundedRectangleBorder(
+                                                    borderRadius:
+                                                        BorderRadius.circular(
+                                                            20)),
+                                                tileColor: Theme.of(context)
+                                                    .colorScheme
+                                                    .primaryContainer,
+                                                title:
+                                                    Text(historyList[index].id),
+                                                subtitle: Text(
+                                                  "Check In : ${history.getClockIn()}\nCheck Out : ${history.getClockOut()}",
+                                                ),
+                                                trailing: Checkbox(
+                                                  value: true,
+                                                  onChanged: (value) {
+                                                    false;
+                                                  },
+                                                ),
+                                              ),
+                                            ));
+                                      });
+                                }),
+                          ),
+                        ),
+                      ),
                     ],
                   ),
                 ),
